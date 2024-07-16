1c136418890c0c09a0e1c0059929a3eeb797b98d
KamalaKannanSF4101 <kamal.sf4101@gmail.com>
2024-07-16 15:35:01 +0530

    KB samples added for SfTreeView

---
+++ MauiTreeView/Helper/Behavior.cs
@@ -0,0 +1,42 @@
+﻿using Syncfusion.Maui.TreeView;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiTreeView
+{
+    public class Behavior : Behavior<SfTreeView>
+    {
+        SfTreeView TreeView;
+        protected override void OnAttachedTo(SfTreeView treeView)
+        {
+            TreeView = treeView;
+            TreeView.SelectionChanging += OnTreeView_SelectionChanging;
+            base.OnAttachedTo(treeView);
+        }
+        private void OnTreeView_SelectionChanging(object sender, Syncfusion.Maui.TreeView.ItemSelectionChangingEventArgs e)
+        {
+            if (TreeView.SelectionMode == Syncfusion.Maui.TreeView.TreeViewSelectionMode.Single)
+            {
+                if (e.AddedItems.Count > 0)
+                {
+                    var item = e.AddedItems[0] as FileManager;
+                    item.LabelColor = Colors.Red;
+                }
+                if (e.RemovedItems.Count > 0)
+                {
+                    var item = e.RemovedItems[0] as FileManager;
+                    item.LabelColor = Colors.Black;
+                }
+            }
+        }
+        protected override void OnDetachingFrom(SfTreeView bindable)
+        {
+            TreeView.SelectionChanging -= OnTreeView_SelectionChanging;
+            TreeView = null;
+            base.OnDetachingFrom(bindable);
+        }
+    }
+}
